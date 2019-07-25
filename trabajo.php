a1104ce8589bdfb27aefca4dfa7c1143d194bdb6
LisandroMarcho <djlm2140@gmail.com>
2019-07-24 22:32:38 -0300

    Renderizado básico de archivos Markdown
    Cuando abris un trabajo, verifica si está el archivo, si tiene contenido
    y si está en la base de datos

---
+++ trabajo.php
@@ -0,0 +1,50 @@
+<?php
+include_once("./conx.php");
+include_once("./includes/Parsedown.php");
+
+$parse = new Parsedown();
+$error = false;
+
+if(!isset($_GET["idtrabajo"])) $error = "¡Ups! Algo ha fallado... <br> (No se pudo encontrar el trabajo)";
+else $idtrabajo = mysqli_real_escape_string($link, $_GET["idtrabajo"]);
+
+//El "@" suprime los errores y alertas
+$archivo = @file_get_contents("./files/$idtrabajo.md");
+if(!$archivo && !strlen($archivo)){
+    $error = "¡Ups! Algo ha fallado... <br> (No hay archivo que abrir)";
+}
+
+if(!$error){
+    $query = "SELECT * FROM trabajos WHERE idtrabajo = $idtrabajo";
+    $trabajo = mysqli_query($link, $query);
+    if(mysqli_num_rows($trabajo) == 1){
+        $trabajo = mysqli_fetch_array($trabajo);
+        $materia = mysqli_fetch_array(mysqli_query($link, "SELECT nom FROM materias WHERE idmateria = $trabajo[1]"));
+    }else $error = "¡Ups! Algo ha fallado... <br> (Error en la Base de Datos)";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <title><?php echo (!$error ? $trabajo[2] : "¡Ups!"); ?> - Lisandro Marchionni</title>
+</head>
+<body>
+    <nav>
+
+    </nav>
+    <div class="md">
+        <?php
+        if($error) echo $error;
+        else{
+            echo $parse->text($archivo);
+        }
+        ?>
+    </div>
+</body>
+</html>
